d7fd050cd2b22a065e4b7035edd7a3763a1485fc
uakari89 <56934409+uakari89@users.noreply.github.com>
2019-12-03 22:24:47 +0000

    Testing JS

---
+++ popup.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+
+        var containers = document.getElementsByClassName("planetContainer");
+        Array.from(containers).forEach(function(){
+          console.log("wpisuje");
+        //  container.onclick = function(){
+
+      //    var mercDesc = document.getElementsByClassN("mercuryDesc");
+
+      //    if(mercDesc.style.display == "block")
+      //    mercDesc.style.display = "none";
+      //    else mercDesc.style.display = "block"; }
+        });
+
+
+
+
+
+
+}
